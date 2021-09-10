367b72728d7ee006ecebb410e470f7b9331296a2
Timo Dokter <timo.dokter@student.rocvf.nl>
2021-09-10 09:50:17 +0200

    question 1 and 2 are done

--- OnderwijsquizUserInputProject2/app/src/main/java/com/example/android/onderwijsquiz_userinputproject2/MainActivity.java
+++ OnderwijsquizUserInputProject2/app/src/main/java/com/example/android/onderwijsquiz_userinputproject2/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.android.onderwijsquiz_userinputproject2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /*vraag 1*/
+    String givenAnswerQ1;
+    String goodAnswerQ1;
+    boolean isAnswerQ1Correct = false;
+
+    public boolean CheckQ1() {
+        goodAnswerQ1 = "1945";
+        EditText antwoord1 = findViewById(R.id.vraag1_antwoord);
+        givenAnswerQ1 = antwoord1.getText().toString().toLowerCase(Locale.ROOT);
+
+        isAnswerQ1Correct = givenAnswerQ1.equals(goodAnswerQ1);
+        return isAnswerQ1Correct;
+    }
+
+    /*vraag 2*/
+    String givenAnswerQ2;
+    String goodAnswerQ2;
+    Boolean isAnswerQ2Correct = false;
+
+    public boolean CheckQ2() {
+        goodAnswerQ2 = "air force one";
+        EditText antwoord2 = findViewById(R.id.vraag2_antwoord);
+        givenAnswerQ2 = antwoord2.getText().toString().toLowerCase(Locale.ROOT);
+
+        isAnswerQ2Correct = givenAnswerQ2.equals(goodAnswerQ2);
+        return isAnswerQ2Correct;
+    }
+
+    /*vraag 3*/
+
 }
