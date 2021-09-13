f2ea0a8528ba7fd54c1cb75a1477e6026168be51
Timo Dokter <timo.dokter@student.rocvf.nl>
2021-09-13 15:16:48 +0200

    lesson 1 is done

---
+++ MultiScreenApp/Miwok/app/src/main/java/com/example/android/miwok/NumbersClickListener.java
@@ -0,0 +1,10 @@
+package com.example.android.miwok;
+
+import android.view.View;
+import android.widget.Toast;
+
+public class NumbersClickListener implements View.OnClickListener {
+
+    @Override
+    public void onClick(View view) {Toast.makeText(view.getContext(), "Open list of numbers", Toast.LENGTH_SHORT).show();}
+}
