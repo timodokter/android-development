a7ae640441ffbc930acf6839e04af5e8039305f1
Timo Dokter <timo.dokter@student.rocvf.nl>
2021-09-14 11:19:13 +0200

    lesson 2 is finished

--- MultiScreenApp/Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ MultiScreenApp/Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,12 +3,33 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
+
+        ArrayList<Word> words = new ArrayList<>();
+        words.add(new Word("one", "lutti"));
+        words.add(new Word("two", "otiiko"));
+        words.add(new Word("three", "tolookosu"));
+        words.add(new Word("four", "oyyisa"));
+        words.add(new Word("five", "massokka"));
+        words.add(new Word("six", "temmokka"));
+        words.add(new Word("seven", "kenekaku"));
+        words.add(new Word("eight", "kawinta"));
+        words.add(new Word("nine", "wo'e"));
+        words.add(new Word("ten", "na'aacha"));
+
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        ListView listView = findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
     }
 }
